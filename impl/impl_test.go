290a60870db57fca99ca6b4fcd0bb559152c5659
andig <cpuidle@gmx.de>
2019-07-04 21:20:01 +0200

    Add tests

--- impl/impl_test.go
+++ impl/impl_test.go
@@ -1,8 +1,14 @@
 package impl
 
 import (
-	"github.com/crabmusket/gosunspec/spi"
+	"encoding/binary"
+	"math"
 	"testing"
+
+	sunspec "github.com/crabmusket/gosunspec"
+	"github.com/crabmusket/gosunspec/smdx"
+	"github.com/crabmusket/gosunspec/spi"
+	"github.com/crabmusket/gosunspec/typelabel"
 )
 
 func TestCompletePointInterface(t *testing.T) {
@@ -15,4 +21,75 @@ func TestCompleteDevice(t *testing.T) {
 
 func TestCompleteArray(t *testing.T) {
 	_ = spi.ArraySPI((&array{}))
+}
+
+func TestNotImplemented(t *testing.T) {
+	cases := []struct {
+		e bool
+		v interface{}
+	}{
+		{false, float32(0)},
+		{true, math.Float32frombits(0x7fc00000)},
+		{false, int16(0)},
+		{false, int32(0)},
+		{false, int64(0)},
+		{true, int16(-0x8000)},
+		{true, int32(-0x80000000)},
+		{true, int64(-0x8000000000000000)},
+		{false, uint16(0)},
+		{false, uint32(0)},
+		{false, uint64(0)},
+		{true, uint16(0xFFFF)},
+		{true, uint32(0xFFFFFFFF)},
+		{true, uint64(0xFFFFFFFFFFFFFFFF)},
+		{false, sunspec.Ipaddr{0xFF, 0xFF, 0xFF, 0xFF}},
+		{false, sunspec.Ipv6addr{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}},
+		{true, sunspec.Ipaddr{0, 0, 0, 0}},
+		{true, sunspec.Ipv6addr{0, 0, 0, 0, 0, 0}},
+		{false, sunspec.Eui48{0, 0, 0, 0, 0, 0}},
+		{true, sunspec.Eui48{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}},
+		{false, sunspec.Bitfield16(0)},
+		{false, sunspec.Bitfield32(0)},
+		{true, sunspec.Bitfield16(0xFFFF)},
+		{true, sunspec.Bitfield32(0xFFFFFFFF)},
+		{false, sunspec.Enum16(0)},
+		{false, sunspec.Enum32(0)},
+		{true, sunspec.Enum16(0xFFFF)},
+		{true, sunspec.Enum32(0xFFFFFFFF)},
+	}
+
+	for _, c := range cases {
+		p := point{
+			err:   nil,
+			value: c.v,
+		}
+
+		if c.e != p.NotImplemented() {
+			t.Errorf("expected %v, got %v", c.e, c.v)
+		}
+
+		if v, ok := p.Value().(sunspec.NotImplemented); !ok {
+			t.Errorf("expected sunspec.NotImplemented, got %v", v)
+		}
+	}
+}
+
+func TestMarshalEui48(t *testing.T) {
+	p := point{
+		err: nil,
+		smdx: &smdx.PointElement{
+			Type: typelabel.Eui48,
+		},
+	}
+
+	p.Unmarshal([]byte{1, 2, 3, 4, 5, 6, 7, 8})
+
+	v := p.Value().(sunspec.Eui48)
+	if binary.BigEndian.Uint64(v[:]) != 0x0102030405060708 {
+		t.Errorf("unexpected value result, got %v", v)
+	}
+
+	if v := p.MarshalXML(); v != "03:04:05:06:07:08" {
+		t.Errorf("unexpected xml result, got %v", v)
+	}
 }
